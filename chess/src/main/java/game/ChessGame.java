c321e520910ea3dcea3f2ffc3454201751b91ca0
ethanp <ethanp@utexas.edu>
2015-08-24 01:08:03 -0500

    SLF4J over Logback with string formatting example

--- chess/src/main/java/game/ChessGame.java
+++ chess/src/main/java/game/ChessGame.java
@@ -2,6 +2,8 @@ package game;
 
 
 import ai.Opponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Scanner;
 import java.util.Set;
@@ -19,6 +21,8 @@ public class ChessGame {
     /** MAIN **/
 
     public static void main(String[] args) {
+        final Logger logger = LoggerFactory.getLogger(ChessGame.class);
+        logger.error("STARTING! {}", logger);
         ChessGame.consoleInterpreted();
     }
 
